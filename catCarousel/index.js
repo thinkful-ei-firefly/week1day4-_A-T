3e5c6209bd42682ca925d3cbe0fae64538017055
Ramarduk <ramarduk@gmail.com>
2019-07-11 12:59:42 -0600

    catCarousel

---
+++ catCarousel/index.js
@@ -0,0 +1,13 @@
+'use strict';
+function handleThumbnailClicks() {
+  $('.thumbnail').on('click', function(event) {
+    const imgSrc = $(this).find('img').attr('src');  
+    const imgAlt = $(this).find('img').attr('alt');
+      
+    $('.hero img').attr('src', imgSrc).attr('alt', imgAlt);
+      
+    
+  });
+}
+  
+$(handleThumbnailClicks);
---
+++ traversalManipulation/index.js
@@ -0,0 +1,7 @@
+'use strict';
+function doHelloWorld() {
+  $('.js-hide-it').addClass('hidden');
+  $('.js-hello-world').text('hello world from JS');
+}
+  
+doHelloWorld();
